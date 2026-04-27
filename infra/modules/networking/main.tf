e5d0204a19669f22fe03275bf9c2fcfeccb8dd81
Steven Vilbert <stevenvilbert@Stevens-MacBook-Pro.local>
2026-04-27 17:57:01 -0400

    removing duplicate resource name

--- infra/modules/networking/main.tf
+++ infra/modules/networking/main.tf
@@ -3,7 +3,7 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_subnetwork" "subnet1" {
-  name          = "moonpay-vpc"
+  name          = "moonpay-subnet"
   ip_cidr_range = "10.16.0.0/22"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.id
