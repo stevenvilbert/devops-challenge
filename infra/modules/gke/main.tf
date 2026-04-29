97aa6f8311eca9573d40776379b86b697b4513e0
Steven Vilbert <stevenvilbert@Stevens-MacBook-Pro.local>
2026-04-28 23:24:29 -0400

    GKE Autoscaling and prompts

--- infra/modules/gke/main.tf
+++ infra/modules/gke/main.tf
@@ -9,9 +9,6 @@ resource "google_container_cluster" "primary" {
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
-  cluster_autoscaling {
-
-  }
 
   #   node_config {
   #     disk_size_gb = var.gke_disk_space
